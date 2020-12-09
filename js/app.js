ebaaa9e683ccb74dd6dfee1bf9e3108e928bceab
Rhythm Biswas <68784427+rhythmdev@users.noreply.github.com>
2020-12-09 18:52:13 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log('Hi Fonni')
+console.log('9' + 9)
+console.log(9 + 9)
+console.log('9'+ 9)
+console.log(60.55)
+
+var name = 'Hm Naim, Shovon, Abir, Akash'
+var age = '25, 27, 30, 21'
+var add = 'Dhaka'
+
+
+console.log( 'Shovon' + ' Knows javascripts. ')
+console.log( ' His age is only ' + "27")
+console.log('Abir',' creating a free javascript course for all')
+console.log(name + ' lives in ' + add)
+
+const check = '15, 25, 23'
+const add = 'Khulna, Barishal'
+
+console.log(check)
+
